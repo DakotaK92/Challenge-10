14607aae08c9553c78f753c0640db201e4ed3800
Dakota.King <dakotaking92@gmail.com>
2024-02-07 22:10:42 -0600

    Push final code

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const inquirer = require('inquirer'); // Looks for the inquirer library
-const fs = require('fs'); // 'File Structure'
-const questions = require('./lib/questions.js'); // Find the questions
-const fileName = "./examples/logo.svg";// Find the logo.svg file
-const setShape = require('./lib/setShapes.js'); // Find the setShapes file
+const inquirer = require('inquirer'); // use inquirer
+const fs = require('fs'); // "file system"
+const questions = require('./lib/questions.js'); // find questions
+const fileName = "./examples/logo.svg"; // find logo.svg
+const setShape = require('./lib/setShape.js') // find setShape.js
 
-// function to create a new svg file using inquirer and file system
-function createLogo(response) {
-    const svg = setShape(response);
-    fs.writeFile(fileName, svg, ()=> console/log('Generated logo.svg'));
+// function to create new svg file using inquirer response and file system
+function createLogo(response) { // create a new logo
+    const svg = setShape(response); // with the new shape
+    fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg')); // with the responses create a logo and log Generated logo
 }
 
-// initialize, ask the questions then create a logo using responses, catch and errors.
-function init() {
-    inquirer
-    .prompt(questions)
-    .then((response) => {
-        createLogo(response);
+// initialize, ask questions then createLogo using responses, catch any errors
+function init() { // initialize
+    inquirer // with inquirer
+    .prompt(questions) // ask questions
+    .then((response) => { // with the responses
+        createLogo(response); // create logo
         })
-    .catch(err => {
-            console.log(err)
+    .catch(err => { //when you hit an error
+            console.log(err) // log error
         });
 }
 
-init();
+init(); 
